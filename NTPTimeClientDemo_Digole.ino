0877449f5339aad7617b9b41c7e9bcfba2895a64
Tony Greening <tgreening@yahoo.com>
2018-03-28 09:20:17 -0400

    Minor updates
    Updates

--- NTPTimeClientDemo_Digole.ino
+++ NTPTimeClientDemo_Digole.ino
@@ -2,11 +2,10 @@
 
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
-#include <WiFiManagerOLED.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
-#include <NTPTimeClient.h>
 #include <ESP8266WiFi.h>
 #include "WundergroundClient.h"
 #include "Time.h"
+#include <Ticker.h>
 #include "images.h"
 #include <SPI.h>
 #include <Ethernet.h>
@@ -14,6 +13,7 @@
 #include "FS.h"
 #include <LinkedList.h>
 #include <ArduinoJson.h>
+#include <TimeClient.h"
 #include <DFPlayer_Mini_Mp3.h>
 #include <SoftwareSerial.h>
 #define _Digole_Serial_I2C_  //To tell compiler compile the special communication only, 
@@ -22,14 +22,14 @@
 DigoleSerialDisp mydisp(&Wire, '\x27'); //I2C:Arduino UNO: SDA (data line) is on analog input pin 4, and SCL (clock line) is on analog input pin 5 on UNO and Duemilanove
 
 
-#define PIN_BUSY 13
-#define PIR_PIN 2
-#define SDC_PIN 14
-#define SDA_PIN 12
+#define PIR_PIN D5
+#define SDC_PIN D2
+#define SDA_PIN D1
 
 boolean busy = true;
-int pirState = LOW;             // we start, assuming no motion detected
+boolean pirState;             // we start, assuming no motion detected
 boolean offset = false;
+const int DISPLAY_UPDATE_SECS = 60; // Update every 1 minute
 
 // Wunderground Settings
 const boolean IS_METRIC = false;
@@ -37,11 +37,17 @@ const String WUNDERGRROUND_API_KEY = "f01878796b8afea5";
 const String WUNDERGRROUND_LANGUAGE = "EN";
 const String WUNDERGROUND_COUNTRY = "MI";
 const String WUNDERGROUND_CITY = "Birmingham";
+const char* ssid     = "Spartans";
+const char* password = "brook13s";
+// flag changed in the ticker function every 10 minutes
+bool readyForWeatherUpdate = false;
+// TimeClient settings
+const float UTC_OFFSET = -5;
 
 // Set to false, if you prefere imperial/inches, Fahrenheit
 WundergroundClient wunderground(IS_METRIC);
 
-SoftwareSerial mp3Serial(12, 14); // RX, TX
+SoftwareSerial mp3Serial(D3, D4); // RX, TX
 
 struct alarm {
   int alarmDay;
@@ -54,11 +60,8 @@ LinkedList<alarm*> alarmsToday = LinkedList<alarm*>();
 String apiKey = "H4ND8D2YBT0GR8KK";
 const char* thingSpeakserver = "api.thingspeak.com";
 WiFiClient client;
-NTPTimeClient timeClient;
-WiFiManagerOLED wifiManager;
+TimeClient timeClient(UTC_OFFSET);
 
-
-NTPClient* ntpClient;
 ESP8266WebServer server(80);
 const char* serverIndex = "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>";
 const char* host = "esp8266-webupdate";
@@ -66,6 +69,7 @@ boolean motion = false;
 int savedDayOfWeek = -1;
 unsigned long startMinute;
 bool checkedHttp = false;
+Ticker ticker;
 
 const char* html = "<html>\n"
                    "<head>\n"
@@ -146,6 +150,8 @@ const char* html = "<html>\n"
                    " htmlString +=\"<option value=\\\"2\\\">2 AM</option>\\n\";\n"
                    " htmlString +=\"<option value=\\\"3\\\">3 AM</option>\\n\";\n"
                    " htmlString +=\"<option value=\\\"4\\\">4 AM</option>\\n\";\n"
+
+
                    " htmlString +=\"<option value=\\\"5\\\">5 AM</option>\\n\";\n"
                    " htmlString +=\"<option value=\\\"6\\\">6 AM</option>\\n\";\n"
                    " htmlString +=\"<option value=\\\"7\\\">7 AM</option>\\n\";\n"
@@ -180,40 +186,26 @@ const char* html = "<html>\n"
                    "</body>"
                    "</html>";
 
-
-//SSD1306  display(I2C_DISPLAY_ADDRESS, SDA_PIN, SDC_PIN);
-
 void setup() {
   Serial.begin(115200);
   mydisp.begin();
-  // mydisp.print("uploading start screen now...(1024 bytes)");
   mydisp.setI2CAddress(0x29);  //this function only working when you connect using I2C, from 1 to 127
   mydisp.setRotation(3);
   delay(500);
   SPIFFS.begin();
-  // display.init();
-  // display.clear();
-  pinMode(PIR_PIN, INPUT);
-  pinMode(PIN_BUSY, INPUT);
-  wifiManager.autoConnect("WeatherStationAP");
-  //mydisp.drawStr(0, 0, "Setting up Wifi.....");
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-    Serial.print ( "." );
-  }
-  //mydisp.drawStr(0, 1, "Updating Time Client....");
-  updateTimeClient();
+  pirState = digitalRead(PIR_PIN);
+  WiFi.begin(ssid, password);
+  updateTime();
 
   postThingSpeak(0);
   if (MDNS.begin("esp8266")) {
     Serial.println("MDNS responder started");
   }
   Serial.println("Checking alarms array...");
-  //mydisp.clearScreen();
-  //mydisp.drawStr(0, 2, "  Reading File...");
 
   readFile();
   server.on("/", HTTP_GET, []() {
+    yield();
     server.sendHeader("Connection", "close");
     server.sendHeader("Access-Control-Allow-Origin", "*");
     Serial.println("Serving up HTML...");
@@ -221,6 +213,7 @@ void setup() {
     Serial.println("Done serving up HTML...");
   });
   server.on("/data", HTTP_GET, []() {
+    yield();
     Serial.printf("get heap size start: %u\n", ESP.getFreeHeap());
     DynamicJsonBuffer jsonBuffer;
     JsonArray& array = jsonBuffer.createArray();
@@ -238,6 +231,7 @@ void setup() {
     Serial.printf("get heap size end: %u\n", ESP.getFreeHeap());
   });
   server.on("/update", HTTP_POST, []() {
+    yield();
     Serial.printf("post heap size start: %u\n", ESP.getFreeHeap());
     HTTPUpload& upload = server.upload();
     String noAlarmsStr = server.arg("noAlarms");
@@ -264,18 +258,100 @@ void setup() {
   });
   server.begin();
   MDNS.addService("http", "tcp", 80);
-  mp3Serial.begin (9600);
- // mydisp.drawStr(0, 4, "Setting up mp3 player");
+  Serial.println("Setting up mp3 player");
   mp3_set_serial (mp3Serial);
   // Delay is required before accessing player. From my experience it's ~1 sec
-  delay(1000);
-  mp3_set_volume (20);
+  mp3_set_volume(30);
+  delay(500);
+  timeAndAlarms();
+  delay(500);
+  weatherUpdate();
+  delay(500);
+  int makeItRight = 61 - second();
+  delay(makeItRight * 1000);
+  ticker.attach(DISPLAY_UPDATE_SECS, timeAndAlarms);
 }
 
-void updateTimeClient() {
-  timeClient.setupByIP();
-  ntpClient = timeClient.getNTPClient();
-  updateNtpClient(ntpClient);
+void timeAndAlarms() {
+  showTime();
+  for (int i = 0; i < alarmsToday.size(); i++) {
+    alarm* n = alarmsToday.get(i);
+    if (n->alarmHour == hour()) {
+      if (n->alarmMinute == minute() && n->alarmEnabled) {
+        motion = false;
+        pinMode(PIR_PIN, INPUT);
+        delay(200);
+        pirState = digitalRead(PIR_PIN);
+        Serial.println(pirState);
+        mp3_play();
+        int repeat = 0;
+        while (n->alarmMinute == minute() && motion == false ) {
+          busy = digitalRead(PIR_PIN);
+          if (busy != pirState) {
+            yield();
+            Serial.println("motion detected...");
+            if (repeat++ > 5) {
+              motion = true;
+              mp3_stop();
+              delay(200);
+              pinMode(PIR_PIN, OUTPUT);
+              digitalWrite(PIR_PIN, 1);
+              yield();
+            }
+          }
+          else {
+            mp3_play();
+          }
+        }
+        alarmsToday.remove(i);
+      }
+      motion = false;
+    }
+  }
+  if (minute() % 30 == 0) {
+    weatherUpdate();
+    delay(1000);
+    int makeItRight = 61 - second();
+    delay(makeItRight * 1000);
+  }
+  if (minute() == 10) {
+    updateTime();
+    delay(1000);
+    int makeItRight = 61 - second();
+    delay(makeItRight * 1000);
+
+  }
+}
+
+void checkWifi() {
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay (500);
+    Serial.print ( "." );
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void updateTime() {
+  int timeClientRetry = 0;
+  long currentTime = now();
+  checkWifi();
+  timeClient.updateTime();
+  setTime(timeClient.getCurrentEpoch());
+  while ((year() == 1970 || year() == 2106) & timeClientRetry < 5 ) {
+    checkWifi();
+    timeClient.updateTime();
+    setTime(timeClient.getCurrentEpoch());
+    Serial.println("Retrying time update....");
+    delay(500);
+    timeClientRetry ++;
+  }
+  if (timeClientRetry == 5) {
+    setTime(currentTime);
+  }
 }
 
 void updateFile(LinkedList<alarm*> alarms) {
@@ -335,85 +411,6 @@ void readFile() {
 
 }
 
-void updateNtpClient(NTPClient* ntpClient) {
-  timeClient.updateTime();
-  setTime(ntpClient->getEpochTime());
-  while (year() == 1970) {
-    Serial.println("Updating time....");
-    timeClient.updateTime();
-    yield();
-    setTime(ntpClient->getEpochTime());
-  }
-}
-
-void loop() {
-  //  Serial.printf("loop heap size start: %u\n", ESP.getFreeHeap());
-  if (hour() == 3 && !offset) {
-    offset = true;
-    updateTimeClient();
-    Serial.println("Updating time....");
-    updateNtpClient(ntpClient);
-    yield();
-    postThingSpeak(2);
-  }
-  yield();
-  if (savedDayOfWeek != weekday()) {
-    offset = false;
-    postThingSpeak(1);
-    setTodaysAlarms();
-    yield();
-    Serial.printf("Current time %s \n", timeClient.getFormattedTime().c_str());
-    savedDayOfWeek = weekday();
-    wunderground.updateForecast(WUNDERGRROUND_API_KEY, WUNDERGRROUND_LANGUAGE, WUNDERGROUND_COUNTRY, WUNDERGROUND_CITY);
-    yield();
-  }
-  showTime();
-  for (int i = 0; i < alarmsToday.size(); i++) {
-    alarm* n = alarmsToday.get(i);
-    if (n->alarmHour == hour()) {
-      if (n->alarmMinute == minute()) {
-        pirState = digitalRead(PIR_PIN) ;
-        mp3_next();
-        delay(200);
-        busy = digitalRead(PIN_BUSY);
-        Serial.print("Motion: ");
-        Serial.print(pirState);
-        Serial.print(" Busy: ");
-        Serial.println(busy);
-        int repeatCount = 0;
-        while (n->alarmMinute == minute() && motion == false && n->alarmEnabled) {
-          Serial.println("Alarm!");
-          mp3_next();
-          busy = digitalRead(PIN_BUSY);
-          Serial.print("Motion: ");
-          Serial.print(pirState);
-          Serial.print(" Busy: ");
-          Serial.println(digitalRead(PIR_PIN));
-          if (digitalRead(PIR_PIN) != pirState) {
-            Serial.println("motion detected...");
-            if (repeatCount++ > 2) {
-              mp3_stop();
-              motion = true;
-            }
-          }
-          else {
-            Serial.println("Playing....");
-            mp3_play();
-          }
-          delay(500);
-          showTime();
-        }
-        alarmsToday.remove(i);
-      }
-      motion = false;
-    }
-  }
-  startMinute = minute();
-  while (minute() == startMinute) {
-    server.handleClient();
-  }
-}
-
 String padInt(int value, int retLength) {
   String padding = "00";
   String valueString = String(value);
@@ -424,58 +421,74 @@ String padInt(int value, int retLength) {
 }
 
 void showTime() {
-    Serial.println("Hour: " + hour());
-    if (hour() > 20 || hour() < 8) {
-      mydisp.backLightBrightness(66);
-    } else {
-      mydisp.backLightBrightness(90);
-    }
-    mydisp.setFont(200);
-    mydisp.setPrintPos(1, 0);
-    if (hour() > 12) {
-      mydisp.print(String(hour() - 12) + ":" + padInt(minute(), 2) );
-    } else {
-      mydisp.print(String(hour()) + ":" + padInt(minute(), 2) );
+  Serial.println("Showing Time....");
+  if (hour() > 20 || hour() < 8) {
+    mydisp.backLightBrightness(25);
+  } else {
+    mydisp.backLightBrightness(75);
+  }
+  mydisp.setFont(200);
+  mydisp.setPrintPos(1, 0);
+  if ((hour() == 1 || hour() == 13) && minute() == 1) {
+    mydisp.setRotation(0);
+    mydisp.clearScreen();
+    delay(50);
+    mydisp.setRotation(3);
+    delay(50);
+  }
+  if (hour() == 0) {
+    mydisp.print("12:" + padInt(minute(), 2) );
+  } else if (hour() > 12) {
+    mydisp.print(String(hour() - 12) + ":" + padInt(minute(), 2) );
+  } else {
+    mydisp.print(String(hour()) + ":" + padInt(minute(), 2) );
 
-    }
-    mydisp.setFont(120);
-    mydisp.setPrintPos(24, 0);
-    if (hour() > 12) {
-      mydisp.print("PM");
-    } else {
-      mydisp.print("AM");
-    }
-    //mydisp.print(timeClient.getFormattedTime().substring(0, 5));
-    mydisp.setFont(201);
+  }
+  mydisp.setFont(120);
+  mydisp.setPrintPos(28, 0);
+  if (hour() > 11) {
+    mydisp.print("PM");
+  } else {
+    mydisp.print("AM");
+  }
+  mydisp.setFont(201);
+  delay(500);
+}
 
-    String day = wunderground.getForecastTitle(0).substring(0, 3);
-    day.toUpperCase();
-    mydisp.setPrintPos(0, 4);
-    mydisp.print(day);
-    mydisp.setPrintPos(0, 5);
-    mydisp.print( wunderground.getForecastLowTemp(0) + "|" + wunderground.getForecastHighTemp(0));
-    displayWeatherImage(0, 130, wunderground.getForecastIcon(0));
-    day = wunderground.getForecastTitle(2).substring(0, 3);
-    day.toUpperCase();
-    mydisp.setPrintPos(7, 4);
-    mydisp.print(day);
-    mydisp.setPrintPos(7, 5);
-    mydisp.print( wunderground.getForecastLowTemp(2) + "|" + wunderground.getForecastHighTemp(2));
-    displayWeatherImage(120, 130, wunderground.getForecastIcon(2));
-    day = wunderground.getForecastTitle(4).substring(0, 3);
-    day.toUpperCase();
-    mydisp.setPrintPos(14, 4);
-    mydisp.print(day);
-    mydisp.setPrintPos(14, 5);
-    mydisp.print( wunderground.getForecastLowTemp(4) + "|" + wunderground.getForecastHighTemp(4));
-    displayWeatherImage(220, 130, wunderground.getForecastIcon(4));
-    yield();
-    mydisp.setFont(10);
-    mydisp.setPrintPos(40, 24);
-    mydisp.print(WiFi.localIP().toString());
+void showWeather() {
+  Serial.println("Showing Weather....");
+  mydisp.setFont(201);
+  yield();
+  String day = wunderground.getForecastTitle(0).substring(0, 3);
+  day.toUpperCase();
+  mydisp.setPrintPos(0, 4);
+  mydisp.print(day);
+  mydisp.setPrintPos(0, 5);
+  mydisp.print( wunderground.getForecastLowTemp(0) + "|" + wunderground.getForecastHighTemp(0));
+  displayWeatherImage(0, 130, wunderground.getForecastIcon(0));
+  day = wunderground.getForecastTitle(2).substring(0, 3);
+  day.toUpperCase();
+  mydisp.setPrintPos(7, 4);
+  mydisp.print(day);
+  mydisp.setPrintPos(7, 5);
+  mydisp.print( wunderground.getForecastLowTemp(2) + "|" + wunderground.getForecastHighTemp(2));
+  displayWeatherImage(120, 130, wunderground.getForecastIcon(2));
+  day = wunderground.getForecastTitle(4).substring(0, 3);
+  day.toUpperCase();
+  mydisp.setPrintPos(14, 4);
+  mydisp.print(day);
+  mydisp.setPrintPos(14, 5);
+  mydisp.print( wunderground.getForecastLowTemp(4) + "|" + wunderground.getForecastHighTemp(4));
+  displayWeatherImage(220, 130, wunderground.getForecastIcon(4));
+  yield();
+  mydisp.setFont(10);
+  mydisp.setPrintPos(40, 24);
+  mydisp.print(WiFi.localIP().toString());
+  yield();
 }
 
 void postThingSpeak(int postValue) {
+  checkWifi();
   if (client.connect(thingSpeakserver, 80)) { // "184.106.153.149" or api.thingspeak.com
     String postStr = apiKey;
     postStr += "&field1=";
@@ -496,7 +509,6 @@ void postThingSpeak(int postValue) {
 }
 
 void setTodaysAlarms() {
-  Serial.print("Day of week: ");
   Serial.println(weekday());
   Serial.print("Month: ");
   Serial.println(month());
@@ -505,6 +517,7 @@ void setTodaysAlarms() {
   Serial.print("Year: ");
   Serial.println(year());
   alarmsToday = LinkedList<alarm*>();
+
   for (int i = 0; i < alarms.size(); i++) {
     alarm* n = alarms.get(i);
     if (n->alarmDay ==  weekday()) {
@@ -529,5 +542,22 @@ void displayWeatherImage(int x, int y, String iconText) {
   if (iconText == "W") mydisp.drawBitmap256(x, y, 50, 50, sleet);
   if (iconText == "R") mydisp.drawBitmap256(x, y, 50, 50, chancerain);
   if (iconText == "0") mydisp.drawBitmap256(x, y, 50, 50, tstorms);
+}
+
+void weatherUpdate() {
+  updateTime();
+  Serial.println("Updating Weather....");
+  postThingSpeak(2);
+  setTodaysAlarms();
+  Serial.printf("Current time %s \n", timeClient.getFormattedTime().c_str());
+  Serial.println("Updating forecast.....");
+  wunderground.updateForecast(WUNDERGRROUND_API_KEY, WUNDERGRROUND_LANGUAGE, WUNDERGROUND_COUNTRY, WUNDERGROUND_CITY);
+  Serial.println("Done updating forecast.....");
+  postThingSpeak(1);
+  showWeather();
+}
+
+void loop() {
+  server.handleClient();
 }
 
